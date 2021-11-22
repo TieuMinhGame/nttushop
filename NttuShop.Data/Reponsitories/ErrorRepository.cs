ea912a11bb34d5dd9f871c39324c2704c03685a2
TieuMinhLapTrinh <minhreuspro123@gmail.com>
2021-11-22 18:24:04 +0700

    delete some files and write api base

---
+++ NttuShop.Data/Reponsitories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using NttuShop.Data.Infrastructure;
+using NttuShop.Model.Models;
+
+namespace NttuShop.Data.Reponsitories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
